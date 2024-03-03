2968769f68121b8213f7eab5b24d55ba7e1eee9f
DarriMT <89972345+DarriMT@users.noreply.github.com>
2024-03-03 20:40:48 +0000

    Update dimCalendar_publish.sql

--- Gagnalíkan/Calander/dimCalendar_publish.sql
+++ Gagnalíkan/Calander/dimCalendar_publish.sql
@@ -9,41 +9,42 @@ BEGIN
         SELECT 1
         FROM [H10].[dimCalendar_stg]
         WHERE [rowBatchKey] = @BatchId
-              AND ([calanderDate] IS NULL OR [year] IS NULL OR [monthNo] IS NULL OR [monthName] IS NULL OR [YYYY-MM] IS NULL OR [week] IS NULL OR [yearWeek] IS NULL)
+              AND ([date] IS NULL OR [year] IS NULL OR [monthNo] IS NULL OR [monthName] IS NULL OR [YYYY-MM] IS NULL OR [week] IS NULL OR [yearWeek] IS NULL)
     )
     BEGIN
     INSERT INTO ErrorLog (TableRowId, TableName, ColumnName, ErrorValue)
     SELECT
-        src.[rowKey] AS TableRowId,
+        src.[id] AS TableRowId,
         'dimCalendar_stg' AS TableName,
     CASE
-        WHEN src.[year] != DATENAME ( yyyy , src.[calanderDate] ) THEN "year"
-        WHEN src.[monthNo] != DATENAME ( MM , src.[calanderDate] ) THEN 'monthNo'
-        WHEN src.[monthName] != DATENAME ( MMMMM , src.[calanderDate] ) THEN 'monthName'
-        WHEN src.[YYYY-MM] != FORMAT(src.[calanderDate], 'yyyy-MM') THEN 'YYYY-MM'
-        WHEN src.[week] != DATEPART(iso_week, src.[calanderDate]) THEN 'week'
-        WHEN src.[yearWeek] != FORMAT(src.[calanderDate], 'yyyy-' + DATEPART(iso_week, src.[calanderDate])) THEN 'yearWeek'
+        WHEN src.[year] != DATENAME ( yyyy , src.[date] ) THEN "year"
+        WHEN src.[monthNo] != DATENAME ( MM , src.[date] ) THEN 'monthNo'
+        WHEN src.[monthName] != DATENAME ( month , src.[date] ) THEN 'monthName'
+        WHEN src.[YYYY-MM] != FORMAT(src.[date], 'YYYY-MM') THEN 'YYYY-MM'
+        WHEN src.[week] != DATEPART(iso_week, src.[date]) THEN 'week'
+        WHEN src.[yearWeek] != FORMAT(src.[date], 'yyyy-' + convert(varchar(2), DATEPART(iso_week, src.[date]))) THEN 'yearWeek'
     END AS ColumnName,
         CASE
-        WHEN src.[year] != DATENAME ( yyyy , src.[calanderDate] ) THEN "Wrong Format"
-        WHEN src.[monthNo] != DATENAME ( MM , src.[calanderDate] ) THEN 'monthNWrong Format'
-        WHEN src.[monthName] != DATENAME ( MMMMM , src.[calanderDate] ) THEN 'Wrong Format'
-        WHEN src.[YYYY-MM] != FORMAT(src.[calanderDate], 'yyyy-MM') THEN 'Wrong Format'
-        WHEN src.[week] != DATEPART(iso_week, src.[calanderDate]) THEN 'Wrong Format'
-        WHEN src.[yearWeek] != FORMAT(src.[calanderDate], 'yyyy-' + DATEPART(iso_week, src.[calanderDate])) THEN 'Wrong Format'
+        WHEN src.[year] != DATENAME ( yyyy , src.[date] ) THEN "year"
+        WHEN src.[monthNo] != DATENAME ( MM , src.[date] ) THEN 'monthNo'
+        WHEN src.[monthName] != DATENAME ( month , src.[date] ) THEN 'monthName'
+        WHEN src.[YYYY-MM] != FORMAT(src.[date], 'yyyy-MM') THEN 'YYYY-MM'
+        WHEN src.[week] != DATEPART(iso_week, src.[date]) THEN 'week'
+        WHEN src.[yearWeek] != FORMAT(src.[date], 'yyyy-' + Convert(varchar(2),DATEPART(iso_week, src.[date]))) THEN 'yearWeek'
         END AS ErrorValue
     FROM [H10].[dimCalendar_stg] src
     WHERE src.[rowBatchKey] = @BatchId
-        AND ([calanderDate] IS NULL OR [year] IS NULL OR [monthNo] IS NULL OR [monthName] IS NULL OR [YYYY-MM] IS NULL OR [week] IS NULL OR [yearWeek] IS NULL)
+        AND ([date] IS NULL OR [year] IS NULL OR [monthNo] IS NULL OR [monthName] IS NULL OR [YYYY-MM] IS NULL OR [week] IS NULL OR [yearWeek] IS NULL)
+    Return 0;
     END;
 
-    -- Proceed with the data manipulation (MERGE) if data quality checks pass
+-- Proceed with the data manipulation (MERGE) if data quality checks pass
     MERGE INTO [H10].[dimCalendar] TRG
     USING [H10].[dimCalendar_stg] SRC
-    ON SRC.rowKey = TRG.rowKey
+    ON SRC.id = TRG.id
     -- and src.rowBatchKey = @BatchId
     WHEN MATCHED THEN
-        UPDATE SET [calanderDate] = src.[calanderDate],
+        UPDATE SET [date] = src.[date],
                    [year] = src.[year],
                    [monthNo] = src.[monthNo],
                    [monthName] = src.[monthName],
@@ -53,8 +54,8 @@ BEGIN
                    [rowBatchKey] = src.[rowBatchKey],
                    [rowModified] = GETUTCDATE()
     WHEN NOT MATCHED THEN
-        INSERT ([rowKey], [calanderDate], [year], [monthNo], [monthName], [YYYY-MM], [week], [yearWeek], [rowBatchKey])
-        VALUES (src.[rowKey], src.[calanderDate], src.[year], src.[monthNo], src.[monthName], src.[YYYY-MM], src.[week], src.[yearWeek], src.[rowBatchKey]);
+        INSERT ([id], [date], [year], [monthNo], [monthName], [YYYY-MM], [week], [yearWeek], [rowBatchKey])
+        VALUES (src.[id], src.[date], src.[year], src.[monthNo], src.[monthName], src.[YYYY-MM], src.[week], src.[yearWeek], src.[rowBatchKey]);
     -- Return 1 to indicate success
     SELECT 1 AS ReturnValue;
 END;
